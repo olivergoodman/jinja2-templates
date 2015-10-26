b1d4ea11a4f56ed583907e4c9f29d3dabcab7773
olivergoodman <ollygreatman@gmail.com>
2015-10-25 19:43:26 -0500

    created templates for index and contact page
    still need to find a way to route to /index and /contact

--- app/views.py
+++ app/views.py
@@ -1,16 +1,40 @@
+from flask import Flask, render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    user = {'firstname': 'Olly', 'lastname' : 'Goodman'}  # fake user
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['lastname'] + '''</h1>
-  </body>
-</html>
-'''
+   page_title = 'Oliver Goodman' 
+   title = 'Home'
+   return render_template('index.html',
+                          title = title,
+                          page_title = page_title)
+
+
+# #how to load new page (contact) ??
+# @app.route('/contact')
+# def contact():
+# 	page_title = 'Contact'
+# 	return render_template('contact.html',
+# 							title = 'Oliver Goodman | Contact'
+# 							page_title = page_title)
+
+
+##example stuff
+# def index():
+#     user = {'nickname': 'Miguel'}  # fake user
+#     posts = [  # fake array of posts
+#         { 
+#             'author': {'nickname': 'John'}, 
+#             'body': 'Beautiful day in Portland!' 
+#         },
+#         { 
+#             'author': {'nickname': 'Susan'}, 
+#             'body': 'The Avengers movie was so cool!' 
+#         }
+#     ]
+#     return render_template("index.html",
+#                            title='Home',
+#                            user=user,
+#                            posts=posts)
