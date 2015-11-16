28972d58e6644e39815c8248ee987309cea785ed
olivergoodman <ollygreatman@gmail.com>
2015-11-16 10:59:43 -0600

    integrating sqlite3 with app

--- app/views.py
+++ app/views.py
@@ -1,23 +1,61 @@
 from flask import Flask, render_template
 from app import app
+import sqlite3
 
+#---------------------DB-------------------------
+conn = sqlite3.connect('page_hits.db')
+print "Opened database successfully";
+
+# #creating table
+# conn.execute('''CREATE TABLE PAGEHITS
+#        (NAME  TEXT  NOT NULL,
+#        HITS   INT PRIMARY KEY   NOT NULL);''')
+# print "Table created successfully";
+
+#adding entries
+# conn.execute("INSERT INTO PAGEHITS (NAME, HITS) \
+#       VALUES ('Home', 0)");
+
+# conn.execute("INSERT INTO PAGEHITS (NAME, HITS) \
+#       VALUES ('Contact', 0)");
+
+# conn.commit()
+# print "Records created successfully";
+
+#displaying
+def displayTable():
+  cursor = conn.execute("SELECT name, hits  from PAGEHITS")
+  for row in cursor:
+    print "NAME = ", row[0]
+    print "HITS = ", row[1], "\n"
+
+def incrementHomeHits():
+  conn.execute("UPDATE PAGEHITS SET HITS = HITS + 1 WHERE NAME = Home")
+
+def incrementContactHits():
+  conn.execute("UPDATE PAGEHITS SET HITS = HITS + 1 WHERE NAME = Contact")
+
+#----------------------------------------------
+  
 @app.route('/')
 @app.route('/index')
 
 def index():
-   page_title = 'Oliver Goodman' 
-   title = 'Home'
-   print 'index was called'
-   return render_template('index.html',
+  page_title = 'Oliver Goodman' 
+  title = 'Home'
+  print 'index was called'
+  incrementHomeHits()
+  displayTable()
+  return render_template('index.html',
                           title = title,
                           page_title = page_title)
 
-
-#how to load new page (contact) ??
 @app.route('/contact')
-def contact():
-	page_title = 'Contact'
-	return render_template('contact.html',
+def contact(): 
+  incrementContactHits()
+  displayTable()
+  page_title = 'Contact'
+  return render_template('contact.html',
 							title = 'Oliver Goodman | Contact',
 							page_title = page_title)
 
